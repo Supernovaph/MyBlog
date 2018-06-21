e55bf72e91150315a857a05ae3416492fe5beb2a
supernovaph <murai.zzz3@gmail.com>
2018-06-21 20:58:52 +0300

    Improved JS

--- static/main.js
+++ static/main.js
@@ -2,15 +2,11 @@ $( "form.to_cart" ).submit(function( event ) {
   event.preventDefault();
   let target = $( event.target );
   let id = target.find("input.product_input").val();
-  $( "#result" ).append( "loading" );
   target.find("input[type^='submit']").after('Loading...');
 
   let posting = $.post( "/shop/buy", {id:id, csrfmiddlewaretoken: target.find("input[name='csrfmiddlewaretoken']").val()});
   
-  posting.done(function( data ) {
-    let content = $( data ).find( "#content" );
-    $( "#result" ).empty().append( content );
-    
+  posting.done(function( data ) { 
     target.find("input[type^='submit']").after('Добавлено в корзину!');
   });
   posting.fail(function(data){
